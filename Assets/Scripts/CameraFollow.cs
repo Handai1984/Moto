543f278b63562ac6ce635e07977090bce1eedc3d
handai <584119509@qq.com>
2018-03-18 20:33:13 +0800

    第一个版本

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+	void Start () {
+        this.transform.position = new Vector3(target.position.x, target.position.y, this.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(target.position.x, target.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public WheelJoint2D rightWheel;
+    public WheelJoint2D leftWheel;
+    private JointMotor2D moto;
+
+    [SerializeField] float motoSpeed;
+    [SerializeField] float motoRotate;
+
+    private Rigidbody2D motobody;
+
+	void Start () {
+        motobody = GetComponent<Rigidbody2D>();
+        moto.maxMotorTorque = 10000;
+       
+		
+	}
+	
+	
+	void Update () {
+        //moto.motorSpeed = -motoSpeed * Input.GetAxis("Vertical");
+        moto.motorSpeed = -motoSpeed * Input.acceleration.y;
+        rightWheel.motor = moto;
+        leftWheel.motor = moto;
+        //motobody.AddTorque(-motoRotate*Input.GetAxis("Horizontal")*Time.deltaTime);
+        motobody.AddTorque(-motoRotate * Input.acceleration.y * Time.deltaTime);
+	}
+}
